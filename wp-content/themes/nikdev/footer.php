52846fdc10af2d99cbe5781bc5c9be29699095c5
Kolya Cherevan <cherevan.n.s@gmail.com>
2020-05-22 15:07:25 +0300

    Adding n-clik

--- wp-content/themes/nikdev/footer.php
+++ wp-content/themes/nikdev/footer.php
@@ -15,7 +15,21 @@
 
 
         <div class="container">
-       <div> <a href="http://www.warlog.ru/" target="_blank"><img border="0" src="http://www.warlog.ru/counter/?i=36" alt="счетчик посещений" title="счетчик посещений" /></a> </div>
+       <div><!-- HotLog -->
+<span id="hotlog_counter"></span>
+<span id="hotlog_dyn"></span>
+<script type="text/javascript"> var hot_s = document.createElement('script');
+hot_s.type = 'text/javascript'; hot_s.async = true;
+hot_s.src = 'http://js.hotlog.ru/dcounter/2588952.js';
+hot_d = document.getElementById('hotlog_dyn');
+hot_d.appendChild(hot_s);
+</script>
+<noscript>
+<a href="http://click.hotlog.ru/?2588952" target="_blank">
+<img src="http://hit5.hotlog.ru/cgi-bin/hotlog/count?s=2588952&im=720" border="0"
+title="HotLog" alt="HotLog"></a>
+</noscript>
+<!-- /HotLog --> </div>
             <!--/* This template is released under the Creative Commons Attribution 3.0 License. Please keep the attribution link below when using for your own project. Thank you for your support. :) If you'd like to use the template without the attribution, you can check out other license options via our website: themes.3rdwavemedia.com */-->
             <small class="copyright">Powered <i class="fa fa-heart"></i> by <a href="   https://t.me/NikDevNik" target="_blank">NikDev</a> </small>
             
