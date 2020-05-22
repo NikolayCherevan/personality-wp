37ed54e0376361bd0d72c62187c93c2ab0e2ced0
Kolya Cherevan <cherevan.n.s@gmail.com>
2020-05-22 15:02:23 +0300

    Adding n-clik

--- wp-content/themes/nikdev/footer.php
+++ wp-content/themes/nikdev/footer.php
@@ -12,7 +12,10 @@
 ?>
 
 <footer class="footer text-center">
+
+
         <div class="container">
+       <div> <a href="http://www.warlog.ru/" target="_blank"><img border="0" src="http://www.warlog.ru/counter/?i=36" alt="счетчик посещений" title="счетчик посещений" /></a> </div>
             <!--/* This template is released under the Creative Commons Attribution 3.0 License. Please keep the attribution link below when using for your own project. Thank you for your support. :) If you'd like to use the template without the attribution, you can check out other license options via our website: themes.3rdwavemedia.com */-->
             <small class="copyright">Powered <i class="fa fa-heart"></i> by <a href="   https://t.me/NikDevNik" target="_blank">NikDev</a> </small>
             
