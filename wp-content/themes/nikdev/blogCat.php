661c4014ca1d9567b4952179d2a147045d89ca57
Kolya Cherevan <cherevan.n.s@gmail.com>
2020-05-23 15:20:28 +0300

    Adding category

--- wp-content/themes/nikdev/blogCat.php
+++ wp-content/themes/nikdev/blogCat.php
@@ -7,46 +7,20 @@ get_header(); ?>
 <div class="blog">
     <div class="container">
         <h2 class="section-title text-center">
-            Категории постов  </h2>
-
-
-        <ul>
-            <?php
-            $args = array(
-                'show_option_all'    => '',
-                'show_option_none'   => __('No categories'),
-                'orderby'            => 'name',
-                'order'              => 'ASC',
-                'style'              => 'ul',
-                'show_count'         => 0,
-                'hide_empty'         => 1,
-                'use_desc_for_title' => 1,
-                'child_of'           => 0,
-                'feed'               => '',
-                'feed_type'          => '',
-                'feed_image'         => '',
-                'exclude'            => '',
-                'exclude_tree'       => '',
-                'include'            => '',
-                'hierarchical'       => true,
-                'title_li'           => __('Categories'),
-                'number'             => NULL,
-                'echo'               => 1,
-                'depth'              => 0,
-                'current_category'   => 0,
-                'pad_counts'         => 0,
-                'taxonomy'           => 'category',
-                'walker'             => 'Walker_Category',
-                'hide_title_if_empty' => false,
-                'separator'          => '<br />',
-            );
-
-            echo '<ul>';
-            wp_list_categories($args);
-            echo '</ul>';
-            the_posts_navigation(); ?>
-        </ul>
-
+            Категории постов </h2>
+        <?php
+        $categories = get_categories($args);
+        $menu .= '<ul сlass = "cat-wrapp">';
+    
+        // iterate through your categories
+        foreach ($categories as $category) {
+            $cat_link = get_category_link($category);
+            // you can also use $category->slug
+            $menu .= '<div class="wrapper"> <div class="'. " cat cat". $category->term_id . '" > </div><div> <a  href = "'  . $cat_link .  '" class="'. "cat". $category->term_id . '">' . $category->name . '</a></div></div>';
+        }
+        $menu .= '</ul>';
+        echo $menu;
+        ?>
     </div>
 
 
